21cf29065a106f3170a234f0977c2f7f2309f715
SonDN <sondoannam202@gmail.com>
2024-10-29 20:50:04 +0700

    refactor: update packages

---
+++ @types/resources.d.ts
@@ -0,0 +1,13 @@
+interface Resources {
+  "common": {
+    "hello": "Welcome",
+    "world": "World",
+    "lng_vi": "Tiếng Việt",
+    "lng_en": "English"
+  },
+  "home": {
+    "explore": "Explore"
+  }
+}
+
+export default Resources;
