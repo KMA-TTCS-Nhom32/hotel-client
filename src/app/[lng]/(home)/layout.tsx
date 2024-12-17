587d4c4e0ed6bc79c42b163dca6e03af823f67d1
SonDN <sondoannam202@gmail.com>
2024-12-18 06:21:05 +0700

    chore: delete unsused components

--- src/app/[lng]/(home)/layout.tsx
+++ src/app/[lng]/(home)/layout.tsx
@@ -1,6 +1,4 @@
 import Header from '@/components/layouts/Header';
-import BranchPage from '@/components/TEST/Components/BranchPage/layouts';
-import IllustrationImage from '@/components/HotelComponents/IllustrationImage';
 import React from 'react';
 
 interface HomeLayoutProps {
