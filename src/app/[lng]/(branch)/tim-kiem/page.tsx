1ab5bfe1f10e081dc0eefdc88182b7b05eab3669
SonDN <sondoannam202@gmail.com>
2025-01-18 05:28:41 +0700

    refactor(actions): :art: update query room-details

--- src/app/[lng]/(branch)/tim-kiem/page.tsx
+++ src/app/[lng]/(branch)/tim-kiem/page.tsx
@@ -1,18 +1,21 @@
 'use client';
 
+import React, { useState } from 'react';
+import { useRequest, useUpdateEffect } from 'ahooks';
+import { useInView } from 'react-intersection-observer';
+
+import styles from './index.module.scss';
+
+import { useTranslation } from '@/i18n/client';
 import Filter from '@/components/SearchRoomPage/RoomList/Filter';
 import RoomCard from '@/components/SearchRoomPage/RoomList/RoomCard';
 import Sidebar from '@/components/SearchRoomPage/Sidebar';
-import React, { useState } from 'react';
-import styles from './index.module.scss';
 import Container from '@/components/Common/Container';
-import { useDebounce, useInViewport, useRequest, useUpdateEffect } from 'ahooks';
 import { RoomSkeleton } from '@/components/SearchRoomPage/RoomList/Skeleton';
 import { getRoomDetailInfiniteService } from '@/services/room-detail';
 import { useSearchBarStore } from '@/stores/search-bar/searchBarStore';
 import { FilterRoomDetailDto, RoomDetail } from '@ahomevilla-hotel/node-sdk';
 import { formatBookingDateTime } from '@/lib/funcs/date';
-import { useTranslation } from '@/i18n/client';
 
 interface SearchPageProps {
   params: { lng: string; slug: string };
@@ -20,22 +23,17 @@ interface SearchPageProps {
 
 export default function ({ params: { lng } }: Readonly<SearchPageProps>) {
   const { bookingTime, customerAmount, province } = useSearchBarStore((state) => state);
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [haveNextPage, setHaveNextPage] = useState(true);
   const [priceRange, setPriceRange] = useState<number[] | undefined>(undefined);
 
   const [roomDetails, setRoomDetails] = useState<RoomDetail[]>([]);
 
-  const {t} = useTranslation(lng, 'searchroom');
+  const { t } = useTranslation(lng, 'searchroom');
 
-  const [inViewport, ratio] = useInViewport(() => document.getElementById('children'), {
-    threshold: [0, 0.25, 0.5, 0.75, 1],
-    root: () => document.getElementById('parent'),
-  });
+  const { ref, inView } = useInView();
 
-  const debounceInViewport = useDebounce(inViewport, { wait: 300 });
-
-  const { loading: getRoomDetailLoading } = useRequest(
+  const { loading: getRoomDetailLoading, refresh } = useRequest(
     () => {
       const { startTime, startDate, endTime, endDate } = formatBookingDateTime(
         bookingTime.checkIn,
@@ -46,6 +44,7 @@ export default function ({ params: { lng } }: Readonly<SearchPageProps>) {
         pageSize: 10,
         page: currentPage,
         filters: JSON.stringify({
+          bookingType: bookingTime.type,
           minPrice: priceRange ? String(priceRange[0]) : undefined,
           maxPrice: priceRange ? String(priceRange[1]) : undefined,
           adults: customerAmount.adult,
@@ -63,21 +62,36 @@ export default function ({ params: { lng } }: Readonly<SearchPageProps>) {
         setRoomDetails([...roomDetails, ...data.data]);
         setHaveNextPage(data.hasNextPage);
       },
-      refreshDeps: [currentPage, priceRange, bookingTime, customerAmount, province],
+      refreshDeps: [currentPage],
     },
   );
 
   useUpdateEffect(() => {
-    if (debounceInViewport && haveNextPage) {
+    if (inView && haveNextPage && !getRoomDetailLoading) {
       setCurrentPage((prev) => prev + 1);
     }
-  }, [debounceInViewport]);
+  }, [inView]);
+
+  useUpdateEffect(() => {
+    setCurrentPage(1);
+    setRoomDetails([]);
+    refresh();
+  }, [priceRange, bookingTime, customerAmount, province]);
 
   return (
-    <Container className={styles.page_container} id='parent'>
-      <div className='flex-grow'>
+    <Container className={styles.page_container}>
+      <div className='min-h-screen flex-grow'>
         <Filter lng={lng} />
         <div className='min-h-screen flex flex-col gap-5'>
+          {currentPage === 1 && getRoomDetailLoading && (
+            <>
+              <RoomSkeleton />
+              <RoomSkeleton />
+              <RoomSkeleton />
+              <RoomSkeleton />
+              <RoomSkeleton />
+            </>
+          )}
           {roomDetails.map((room) => (
             <RoomCard key={room.id} room={room} bookingType={bookingTime.type} t={t} />
           ))}
@@ -96,7 +110,7 @@ export default function ({ params: { lng } }: Readonly<SearchPageProps>) {
         <Sidebar lng={lng} onChangePriceRange={setPriceRange} />
       </div>
 
-      <div id='children' className='h-1' />
+      <div id='children' className='h-1' ref={ref} />
     </Container>
   );
 }
